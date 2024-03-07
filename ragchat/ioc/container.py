e2301364b35cec928fbf444cfd80b031b6d955b9
Dean Phillips <dean.phillips@e-hps.com>
2024-03-06 22:03:28 -0600

    Wired the documents controller to the IoC container and main controller

--- ragchat/ioc/container.py
+++ ragchat/ioc/container.py
@@ -1,5 +1,6 @@
 from dependency_injector import containers, providers
 from ragchat.application.config.config_provider import ConfigProvider
+from ragchat.controllers.documents_controller import DocumentsController
 from ragchat.controllers.utilities_controller import UtilitiesController
 from ragchat.data.collections.in_memory_repository import InMemoryCollectionRepository
 from ragchat.application.use_cases.collections.use_cases import CollectionsUseCases
@@ -7,31 +8,31 @@ from ragchat.controllers.cli import CollectionsController
 from ragchat.controllers.cli import Cli
 from ragchat.data.collections.sqlite_repository import SqLiteCollectionRepository
 
+
 class Container(containers.DeclarativeContainer):
-    config_provider = providers.Singleton(ConfigProvider, config_path='config.json')
+    config_provider = providers.Singleton(ConfigProvider, config_path="config.json")
 
     collection_repository_factory = providers.Factory(
-        SqLiteCollectionRepository,
-        config_provider
+        SqLiteCollectionRepository, config_provider
     )
 
     collection_use_cases_factory = providers.Factory(
-        CollectionsUseCases,
-        collection_repository_factory
+        CollectionsUseCases, collection_repository_factory
     )
 
     collections_controller_factory = providers.Factory(
-        CollectionsController,
-        collection_use_cases_factory
+        CollectionsController, collection_use_cases_factory
     )
 
+    documents_controller_factory = providers.Factory(DocumentsController)
+
     utilities_controller_factory = providers.Factory(
-        UtilitiesController,
-        config_provider
+        UtilitiesController, config_provider
     )
 
     cli_factory = providers.Factory(
         Cli,
         collections_controller_factory,
-        utilities_controller_factory
+        utilities_controller_factory,
+        documents_controller_factory,
     )
